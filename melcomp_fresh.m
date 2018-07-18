f41f4048861c8f944ed850d101e68b6a6c529449
da5nsy <dannygarside@outlook.com>
2018-07-18 18:39:17 +0100

    fiddling away

--- melcomp_fresh.m
+++ melcomp_fresh.m
@@ -2,22 +2,24 @@ clear, clc, close all
 
 % A fresh attempt
 
-%% Load Reflective Things
-
+%% Load Reflectances
 load sur_vrhel
 refs=[87, 93, 94, 134, 137, 138, 65, 19, 24, 140, 141];
-sur_vrhel=sur_vrhel(:,refs);
+T_vrhel=sur_vrhel(:,refs)';
+clear sur_vrhel refs
 
-%% Load/Compute Daylight SPD
+%% Compute Daylight SPD
+
 D_CCT=1./linspace(1/3600,1/25000,20); %non-linear range, aiming to better reproduce observed variation
 load B_cieday
-daylight_spd = GenerateCIEDay(D_CCT,[B_cieday]); %these appear to be linearly upsampled from 10nm intervals
-daylight_spd = daylight_spd./repmat(max(daylight_spd),81,1); %normalise
+T_Dspd = GenerateCIEDay(D_CCT,[B_cieday]); %these appear to be linearly upsampled from 10nm intervals (see 'cieday investigation.m' https://github.com/da5nsy/General-Purpose-Functions/blob/3ee587429e9c4f3dd52d64acd95acf82d7e05f47/cieday%20investigation.m)
+T_Dspd = (T_Dspd./repmat(max(T_Dspd),81,1)); %normalise
+T_Dspd = SplineSpd(S_cieday,T_Dspd,S_vrhel,2); % '2' flag -> Linear interp, linear extend
 
 %% Observer
 
 % Smith-Pokorny, for use with MacLeod Boynton diagram
-load T_cones_sp
+load T_cones_sp %should use a different version (see 'PTB_SP.m': https://github.com/da5nsy/General-Purpose-Functions/blob/3ee587429e9c4f3dd52d64acd95acf82d7e05f47/PTB_SP.m)
 load T_rods
 load T_melanopsin
 
@@ -28,3 +30,9 @@ T_LMSRI=[(SplineCmf(S_cones_sp,T_cones_sp,S_vrhel));...
 S_LMSRI=S_vrhel;
 
 %% Convert to radiance
+
+% PUT IT ALL TOGETHER
+LMSRI = T_Dspd*T_vrhel*T_LMSRI;
+
+%% 
+

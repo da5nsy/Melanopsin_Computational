c8c6be7d65b478a95f3a3df6b959e44f7851247f
da5nsy <dannygarside@outlook.com>
2018-07-19 12:07:08 +0100

    messing around with rotation

--- melcomp_fresh.m
+++ melcomp_fresh.m
@@ -28,6 +28,9 @@ load T_cones_sp %should use a different version (see 'PTB_SP.m': https://github.
 load T_rods
 load T_melanopsin
 
+% For messing with hypothetical spectral sensitivity of melanopsin
+%S_melanopsin(1)=S_melanopsin(1)+30;
+
 % Pull them all together
 T_LMSRI=[(SplineCmf(S_cones_sp,T_cones_sp,S_vrhel));...
     (SplineCmf(S_rods,T_rods,S_vrhel));...
@@ -40,14 +43,55 @@ for i=1:size(T_Dspd,1)
     T_rad(:,:,i) = T_vrhel.*T_Dspd(i,:);
     LMSRI(:,:,i) = T_rad(:,:,i)*T_LMSRI';
     ls(:,:,i)    = LMSToMacBoyn(LMSRI(:,1:3,i)');
-    m(1,:,i)     = LMSRI(:,5,i)./(0.6373*LMSRI(:,1,i)+0.3924*LMSRI(:,2,i)); % not called i because I use that for all the loopz :/
+    
+    m(1,:,i)     = LMSRI(:,5,i)./(0.6373*LMSRI(:,1,i)+0.3924*LMSRI(:,2,i)); 
+    % not called 'i' because I use that for all the loops
+    % used the same scalars for luminance as are in the LMSToMAcBoyn
+    %   function - which relate to the Smith-Pokorny fundamentals
 end
 
-figure, hold on, axis equal, xlim([0 1]), ylim([0 1])
-for i=1:4:size(T_Dspd,1)
-    plot(ls(1,:,i),ls(2,:,i),'o-')
+figure, hold on, axis equal, xlim([0 1]), ylim([0 1]), zlim([0,1])
+for i=1:size(T_Dspd,1)
+    plot3(ls(1,:,i),ls(2,:,i),m(1,:,i),'o-')
+end
+xlabel('l'),ylabel('s'),zlabel('m');
+
+view(188,46)
+
+%% Rotation of data
+clc
+
+m=m(:)';
+lsm=[ls(1:2,:);m]';
+
+% figure, hold on, axis equal, xlim([0 1]), ylim([0 1]), zlim([0,1])
+% plot3(lsm(1,:,:),lsm(2,:,:),lsm(3,:,:),'o')
+
+%rotation matrix
+
+figure, hold on, axis equal, 
+for a=pi/10:pi/40:pi*5
+%a=188; %angle
+rm=...
+    [1,0,0;...
+    0,cos(a),-sin(a);...
+    0,sin(a),cos(a)]; 
+
+lsm_r=lsm*rm';
+
+clf, hold on
+xlim([-1 1]), ylim([-1 1]), zlim([-1,1])
+plot3(lsm(:,1),lsm(:,2),lsm(:,3),'bo')
+plot3(lsm_r(:,1),lsm_r(:,2),lsm_r(:,3),'ro')
+
+view(188,46)
+drawnow
 end
 
+
+
+
+%% %-% %%
 %% Factors
 
 % Calibrating by l or s results in hitting unity in the relevent dimension
@@ -71,20 +115,44 @@ for i=1:4:size(T_Dspd,1)
     title('Yes, it really is meant to just be a single point')
 end
 
-ls5 = ls./ls; %same as above, just being careful
-figure, hold on, axis equal, %xlim([0 1]), ylim([0 1])
-for i=1:4:size(T_Dspd,1)
-    plot(ls5(1,:,i),ls5(2,:,i),'o-')
-    title('Yes, it really is meant to just be a single point')
-end
+% ls5 = ls./ls; %same as above, just being careful
+% figure, hold on, axis equal, %xlim([0 1]), ylim([0 1])
+% for i=1:4:size(T_Dspd,1)
+%     plot(ls5(1,:,i),ls5(2,:,i),'o-')
+%     title('Yes, it really is meant to just be a single point')
+% end
 
 
-%% multiplying direct by m
+%% calibrating by m
 
-ls6 = ls./m;
+ls6 = ls./[m(1,:,:);m(1,:,:)];
+%ls6(2,:)=(1./(ls6(2,:)+0.1245)).^(1/0.44);
 figure, hold on, axis equal, %xlim([0 1]), ylim([0 1])
 for i=1:4:size(T_Dspd,1)
     plot(ls6(1,:,i),ls6(2,:,i),'o-')
 end
+drawnow
 
 % What would this ideally look like?
+
+%% Trying to work out the equation
+
+a=squeeze(ls6(:,4,:));
+
+figure, hold on
+clf, hold on
+scatter(a(1,:),a(2,:))
+x=1:0.1:3.1;
+y=(1./(x.^0.44))-0.1245;
+plot(x,y)
+
+% y2=(1./(y+0.1245)).^(1/0.44);
+% plot(x,y2)
+% %%
+% a2=a;
+% a2(2,:)=(1./(a2(2,:)+0.1245)).^(1/0.44);
+% 
+% figure,
+% scatter(a2(1,:),a2(2,:))
+
+

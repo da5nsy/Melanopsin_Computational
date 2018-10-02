db3a726c73f278322f754bb98e4fc5d1353a8e34
da5nsy <dannygarside@outlook.com>
2018-10-03 00:24:10 +0100

    got the Maloney stuff to the stage of working, but unsure exactly as to how/why it works

--- maloney_practical_example.m
+++ maloney_practical_example.m
@@ -8,52 +8,95 @@
 
 clear, clc, close all
 
-%% Load basis functions and define
+%% For illuminant and reflectance, load basis functions and define:
 % 1. Basis functions
 % 2. Basis weights
 % 3. Full spectra
 
-load B_cieday
-load B_vrhel
+plt_lin = 0;
 
-[E,~,~,e(2),e(3)] = GenerateCIEDay(6500,B_cieday); 
-e(1) = 1;
-e=e';
-%Calculates an SPD, and weights of the basis functions also the weights of
-%the basis functions needed to create that (E(spd) = B_cieday*e)
-% check = isequal(E, B_cieday*e);
+load B_vrhel %basis functions of reflectance measurements
+B_vrhel = B_vrhel(:,1:3); %Let's pretend it only has 3 basis functions
+s = [-1;0.2;0.6]; %weights (arbitrary random picks)
+S = B_vrhel*s; %Spectral reflectance function
 
-B_vrhel = B_vrhel(:,1:3); %Let's pretend it only has 3 dimensions, for simplicity
-s = [-1;0.2;0.6]; %random picks
-S = B_vrhel*s;
+load B_cieday %basis functions of daylight measurements
+B_cieday = SplineSpd(S_cieday,B_cieday,S_vrhel); %interpolate to match sampling range and interval
+S_cieday = S_vrhel;
+[E,~,~,e(2),e(3)] = GenerateCIEDay(6500,B_cieday); %E = Spectral power distribution. e = weights.
+e(1) = 1; %GenerateCIEDay sets e(1) = 1
 
-figure, hold on
-plot(SToWls(S_cieday),E/max(E),'b','LineWidth',4)
-for i =1:3
-plot(SToWls(S_cieday),B_cieday(:,i)*e(i)/max(E),'b--','LineWidth',4-i)
+
+if plt_lin %visualise the basis functions and the full spectra
+    figure, hold on
+    plot(SToWls(S_cieday),E/max(E),'b','LineWidth',4)
+    for i =1:3
+        plot(SToWls(S_cieday),B_cieday(:,i)*e(i)/max(E),'b--','LineWidth',4-i)
+    end
+    
+    plot(SToWls(S_vrhel),S/max(S),'r','LineWidth',5)
+    for i =1:3
+        plot(SToWls(S_vrhel),B_vrhel(:,i)*s(i)/max(S),'r--','LineWidth',4-i)
+    end
+    
+    plot([min(xlim),max(xlim)],[0,0],'k:')
+    legend({'E','B_cieday(1) * e(1)','B_cieday(2) * e(2)','B_cieday(3) * e(3)'...
+        'S','B_vrhel(1) * s(1)','B_vrhel(2) * s(2)','B_vrhel(3) * s(3)'},'Location','best','Interpreter', 'none')
 end
 
-plot(SToWls(S_vrhel),S/max(S),'r','LineWidth',5)
-for i =1:3
-plot(SToWls(S_vrhel),B_vrhel(:,i)*s(i)/max(S),'r--','LineWidth',4-i)
-end
+%% Sensor Spectral Sensitivities
 
-plot([min(xlim),max(xlim)],[0,0],'k:')
-legend({'E','B_cieday(1) * e(1)','B_cieday(2) * e(2)','B_cieday(3) * e(3)'...
-    'S','B_vrhel(1) * s(1)','B_vrhel(2) * s(2)','B_vrhel(3) * s(3)'},'Location','best','Interpreter', 'none')
-
-
-%% 
-
-
-load T_cones_sp
+load T_cones_sp % Load sensor receptivities
+T_cones_sp = SplineCmf(S_cones_sp,T_cones_sp,S_vrhel); %interpolate to match sampling range and interval
+S_cones_sp = S_vrhel;
+%plot(T_cones_sp')
+R = T_cones_sp;
 
 %%
-
 % (eq numbers from second ref)
 
-% Eq 9.3
+% Eq. 9.3
+L = E.*S; %color signal = illum .* reflectance
+p = R*L; %photon catch = sensor sensitivities * color signal
 
-L = E*S;
-%p = L*R;
+% % written alternatively:
+p_alt1 = R*(E.*S); %photon catch = sensor sensitivities * illum .* reflectance
+
+% Eq. 9.4
+% Refers to intrisic colour calculation
+
+% Eq. 9.5
+if ~isequal(E, B_cieday*e')
+    error('Error: SPD does not equal SPD reconstructed from basis functions')
+end
+
+% Eq. 9.6
+if ~isequal(S, B_vrhel*s)
+    error('Error: SRF does not equal SRF reconstructed from basis functions')
+end
+
+% Jumping back to thesis
+% First part of Eq. at bottom of page 63:
+p_alt2 = sum(E.*S.*R'); %with spectra
+p_alt3 = sum(B_cieday*e'.*B_vrhel*s.*R'); %with linear functions and weights
+
+% Second part
+
+for i=1:3
+    for j=1:3
+        for k=1:3
+            g(i,j,k) = sum(B_cieday(:,i).*B_vrhel(:,j).*R(k,:)');
+        end
+    end
+end
+
+p_alt4 = [trace(s*e*g(:,:,1)),trace(s*e*g(:,:,2)),trace(s*e*g(:,:,3))];
+
+% I have no idea why the above works but it does.
+
+
+
+
+
+
 

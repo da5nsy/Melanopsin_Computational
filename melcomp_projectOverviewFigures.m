51cba94d7675d9936141c85e17ab9c6bf9fae3c0
da5nsy <dannygarside@outlook.com>
2018-10-09 20:02:20 +0100

    added print option

--- melcomp_projectOverviewFigures.m
+++ melcomp_projectOverviewFigures.m
@@ -4,8 +4,19 @@
 
 clc, clear, close all
 
+base = 'C:\Users\cege-user\Dropbox\UCL\Ongoing Work\Melanopsin Computational\Project Overview Presentation';
+
+ff = '-dtiff'; %file format
+p  = 0; %print? (aka save)
+
 plot_where = [20,60];
-plot_size  = [700,400];
+plot_size  = [800,375];
+
+%% Start the figure
+
+figure('Position',[plot_where plot_size]), hold on
+set(gca, 'FontSize', 16)
+set(gcf,'defaultLineLineWidth',2)
 
 %% SPDs
 
@@ -18,33 +29,47 @@ load('C:\Users\cege-user\Dropbox\UCL\Data\Reference Data\Granada Data\Granada_da
 T_SPD = final(17:97,:); clear final
 S_SPD = [380,5,81];
 
-figure('Position',[plot_where plot_size]), hold on
 xlim([380 780]); xticks(380:100:780);
 ylim([0 2]); yticks([0,1,2]);
 xlabel('Wavelength'); ylabel('Power');
 
 plot(SToWls(S_SPD),T_SPD(:,103))
+if p
+    print([base,'\','1'],ff)
+end
 
 %% all the SPDs
 plot(SToWls(S_SPD),T_SPD)
+if p
+    print([base,'\','2'],ff)
+end
 
 %%
 
 cla
 [pc.coeff, pc.score, pc.latent, pc.tsquared, pc.explained, pc.mu] = pca(T_SPD');
 
-ylim([-1 1]); yticks([-1,0,1]);
+ylim([-0.3 0.3]); yticks([-0.3,0,0.3]);
 %legend({'PC1','PC2','PC3'},'Location','Southwest')
 fill([min(xlim),max(xlim),max(xlim),min(xlim),min(xlim)],[-1,-1,0,0,-1],...
     'k','LineStyle','none','FaceAlpha','0.03');
 
-%plot(SToWls(S_SPD),pc.COEFF(:,1:3)./max(pc.COEFF(:,1:3)))
+%plot(SToWls(S_SPD),pc.coeff(:,1:3)./max(pc.coeff(:,1:3)))
 
-plot(SToWls(S_SPD),pc.coeff(:,1)./max(pc.coeff(:,1)))
+plot(SToWls(S_SPD),pc.coeff(:,1))
+if p
+    print([base,'\','3'],ff)
+end
 %%
-plot(SToWls(S_SPD),pc.coeff(:,2)./max(pc.coeff(:,2)))
+plot(SToWls(S_SPD),pc.coeff(:,2))
+if p
+    print([base,'\','4'],ff)
+end
 %%
-plot(SToWls(S_SPD),pc.coeff(:,3)./max(pc.coeff(:,3)))
+plot(SToWls(S_SPD),pc.coeff(:,3))
+if p
+    print([base,'\','5'],ff)
+end
 
 %%
 cla
@@ -54,9 +79,12 @@ ylim([0 2]); yticks([0,1,2]);
 recon(:,1) = pc.score(103,:)   * pc.coeff'        + pc.mu; % Reconstructed with all PCs
 recon(:,2) = pc.score(103,1:3) * pc.coeff(:,1:3)' + pc.mu; % Reconstructed with 3 PCs
 
-plot(SToWls(S_SPD), T_SPD(:,103), 'b') % Original data
-plot(SToWls(S_SPD), recon(:,2), 'r--') % 3 PC
+%plot(SToWls(S_SPD), T_SPD(:,103), 'b') % Original data
+plot(SToWls(S_SPD), recon(:,2), 'r') % 3 PC
 % legend({'Original Data','Reconstructed with 1:3'})
+if p
+    print([base,'\','6'],ff)
+end
 
 % figure, plot((pc.score*pc.coeff' + repmat(pc.mu,2600,1))') %all the data reconstructed
 

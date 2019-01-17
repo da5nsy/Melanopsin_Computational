1386fddfca21a1740fd7cfeb9aa6e823e1618cf2
da5nsy <dannygarside@outlook.com>
2019-01-17 17:48:27 +0000

    fixing up optimality, aesthetics

--- Auxiliary Scripts/melcomp_optimality.m
+++ Auxiliary Scripts/melcomp_optimality.m
@@ -10,8 +10,9 @@ catch
     %default values
     ss=1;
     prog=1;
-    t_range = [-200,5,400];
+    t_range = [-100,5,300];
     disp('Using default values');
+    spd = 1;
 end
 
 %Compute melanopic peak sensitivity
@@ -23,7 +24,7 @@ Ip = S_mel(mloc); %Mel peak
 t_range_ex=t_range(1):t_range(2):t_range(3);
 
 for i=t_range_ex
-    pc(i-t_range(1)+1) = melcomp_2(1,1,1,9,NaN,i); %principal components    
+    pc(i-t_range(1)+1) = melcomp_2(spd,1,1,9,NaN,i); %principal components    
     ex(i-t_range(1)+1,:) = pc(i-t_range(1)+1).explained; %explained
     if prog %progress
         disp(i)
@@ -36,11 +37,14 @@ AoI = X_ax(locs); %Areas of interest
 
 %Plot principal components
 plt_firstAndSecondPC = 0;
-plot(Ip+t_range_ex,ex_c,'b','LineWidth',3,'DisplayName','3rd PC weight')
 if plt_firstAndSecondPC
-    plot(Ip+t_range_ex,ex(1:t_range(2):end,1),'r','LineWidth',3,'DisplayName','1st PC weight')
-    plot(Ip+t_range_ex,ex(1:t_range(2):end,2),'g','LineWidth',3,'DisplayName','2nd PC weight')
+    plot(Ip+t_range_ex,ex(1:t_range(2):end,1)/max(ex(1:t_range(2):end,1)),...
+        'r','LineWidth',3,'DisplayName',['1st PC weight /' num2str(max(ex(1:t_range(2):end,1)))])
+    plot(Ip+t_range_ex,ex(1:t_range(2):end,2)/max(ex(1:t_range(2):end,2)),...
+        'g','LineWidth',3,'DisplayName',['2nd PC weight /' num2str(max(ex(1:t_range(2):end,2)))])
 end
+plot(Ip+t_range_ex,ex(1:t_range(2):end,3)/max(ex(1:t_range(2):end,3)),...
+    'b','LineWidth',3,'DisplayName',['3rd PC weight /' num2str(max(ex(1:t_range(2):end,3)))])
 
 plt_melPeakLine = 0;
 if plt_melPeakLine
@@ -51,7 +55,7 @@ end
 plt_thirdPCPeak = 0;
 if plt_thirdPCPeak
     [~,m3] = max(ex(:,3)); %max third principal component
-    plot([m3+Ip+range(1)-1,m3+Ip+range(1)-1],[min(y),max(y)],'DisplayName','3rd PC max')
+    plot([m3+Ip+t_range(1)-1,m3+Ip+t_range(1)-1],[min(y),max(y)],'DisplayName','3rd PC max')
 end
 
 if ss %spectral sensitvities
@@ -64,48 +68,51 @@ if ss %spectral sensitvities
     plot(SToWls(S_cones_sp),T_cones_sp(1,:),'r:','DisplayName','l-cone')    
 end
 
-plt_peakLines = 0;
-if plt_peakLines
-    legend('AutoUpdate','off')
-    for i = 1:length(locs)
-        plot([AoI(i),AoI(i)],[min(ylim),pks(i)],'k')
-    end
-    % figure, plot(X_ax,ex_c) %safety check
-end
+% %broken, and can't work out what it is meant to do
+% plt_peakLines = 0;
+% if plt_peakLines
+%     legend('AutoUpdate','off')
+%     for i = 1:length(locs)
+%         plot([AoI(i),AoI(i)],[min(ylim),pks(i)],'k')
+%     end
+%     % figure, plot(X_ax,ex_c) %safety check
+% end
 
 legend('Location','best')
 axis tight
 
 %% See what the data looks like in 3 dimensions at the peaks pulled out by the previous calcs
 
-plt_viz = 0;
+plt_viz = 1;
 
 if plt_viz
     for i = 1:length(locs)
         figure,
-        melcomp_2(1,1,1,9,'3D',t_range_ex(locs(i)));
+        melcomp_2(spd,1,1,9,'3D',t_range_ex(locs(i)));
         title(Ip+t_range_ex(locs(i)))
     end
 end
 
 %%
-% %Old way which plots the changing shape 
-% for i=range(1):range(2)
-%     figure('units','normalized','outerposition',[0 0 1 1]), hold on
-%     subplot(1,2,1)
-%     melcomp_2(1,1,1,9,'3D',i);
-%     view(0,0)
-%     zlim([0 1])
-%     
-%     subplot(1,2,2)
-%     melcomp_2(1,1,1,9,'3D',i);
-%     view(90,0)
-%     zlim([0 1])
-%     
-%     drawnow
-%     if i~=range(2)
-%         clf
-%     end
-% end
+%Old way which plots the changing shape 
+range = [-100,100];
+
+for i=range(1):range(2)
+    figure('units','normalized','outerposition',[0 0 1 1]), hold on
+    subplot(1,2,1)
+    melcomp_2(1,1,1,9,'3D',i);
+    view(0,0)
+    zlim([0 1])
+    
+    subplot(1,2,2)
+    melcomp_2(1,1,1,9,'3D',i);
+    view(90,0)
+    zlim([0 1])
+    
+    drawnow
+    if i~=range(2)
+        clf
+    end
+end
 
 end

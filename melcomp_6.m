16c6d45815622939641106bedc06a48f1cb742de
da5nsy <dannygarside@outlook.com>
2019-01-21 14:17:05 +0000

    attempt number 2, doing it from the spectral data provided in part 2. This didnt work, but was a neat way to plot the chromaticity diagram

--- melcomp_6.m
+++ melcomp_6.m
@@ -1,15 +1,26 @@
 clear
 clc
 
-%%
+%% Load data
 
-S = [390 1 441]; 
-SToWls(S)
+% Tables from CIE 170-2:2015
 
-T_quantal_PTB = ComputeCIEConeFundamentals(S,10,32,3);
-T_energy_PTB = EnergyToQuanta(S,T_quantal_PTB')';
+% live:     http://files.cie.co.at/813_Tables_CIE_170-2.xls
+% archive:  https://web.archive.org/web/20190121134312/http://files.cie.co.at/813_Tables_CIE_170-2.xls
 
-%%
+T_CIE_MB_10 = xlsread('C:\Users\cege-user\Dropbox\UCL\Data\Colour standards\813_Tables_CIE_170-2.xls','Table 10.6','B2:D90');
+S_CIE_MB_10 = [390,5,89];
 
-figure, plot(T_energy_PTB')
-%figure, plot(T_quantal_PTB')
+%% Plot spectral locus
+
+figure,
+scatter(T_CIE_MB_10(:,1),T_CIE_MB_10(:,3))
+xlim([0.4,1])
+
+%% 
+
+EE = ones(length(T_CIE_MB_10),1);
+
+T_CIE_MB_10'*EE;
+
+

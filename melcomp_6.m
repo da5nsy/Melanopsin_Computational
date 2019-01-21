bb42cb268846fa7b55277b6b371a945a68006765
da5nsy <dannygarside@outlook.com>
2019-01-21 16:25:10 +0000

    try with quantal

--- melcomp_6.m
+++ melcomp_6.m
@@ -1,24 +1,28 @@
 clear
 clc
 
-%%
+%% Generate cone fundamentals
 
 S = [390 1 441]; 
 %SToWls(S)
 
 T_quantal_PTB = ComputeCIEConeFundamentals(S,10,32,3);
-T_energy_PTB = EnergyToQuanta(S,T_quantal_PTB')';
-
-%figure, plot(T_energy_PTB')
+%figure, plot(T_quantal_PTB')
 
 %%
 
 %Scaling factors from Eq. 8.5 from CIE 170-2:2015
-sf = [0.69283932, 0.34967567, 0.05547858];
+sf_q10 = [0.67390486, 0.35656342, 0.06834828]; %quantal 10
+sf_e10 = [0.69283932, 0.34967567, 0.05547858]; %energy 10
 
 %% Test by computing EE co-ordinates
 
-LMS_EE = sum(T_energy_PTB').*sf;
+EEenergy = ones(length(T_quantal_PTB),1); %equi-energy white
+EEquanta = EnergyToQuanta(S,EEenergy);
+
+LMS_EE = sf_q10.*(T_quantal_PTB*EEquanta);
+
+%LMS_EE = sum(T_quantal_PTB').*sf;
 
 l = LMS_EE(1)/(LMS_EE(1)+LMS_EE(2))
 m = LMS_EE(2)/(LMS_EE(1)+LMS_EE(2));
@@ -42,11 +46,12 @@ s = LMS_EE(3)/(LMS_EE(1)+LMS_EE(2))
 
 %% Test by computing spectral co-ordinates
 
-l_spectral = sf(1)*T_energy_PTB(1,:)./(sf(1)*T_energy_PTB(1,:)+sf(2)*T_energy_PTB(2,:));
-m_spectral = sf(2)*T_energy_PTB(2,:)./(sf(1)*T_energy_PTB(1,:)+sf(2)*T_energy_PTB(2,:));
-s_spectral = sf(3)*T_energy_PTB(3,:)./(sf(1)*T_energy_PTB(1,:)+sf(2)*T_energy_PTB(2,:));
+l_spectral = sf_q10(1)*T_quantal_PTB(1,:)./(sf_q10(1)*T_quantal_PTB(1,:)+sf_q10(2)*T_quantal_PTB(2,:));
+m_spectral = sf_q10(2)*T_quantal_PTB(2,:)./(sf_q10(1)*T_quantal_PTB(1,:)+sf_q10(2)*T_quantal_PTB(2,:));
+s_spectral = sf_q10(3)*T_quantal_PTB(3,:)./(sf_q10(1)*T_quantal_PTB(1,:)+sf_q10(2)*T_quantal_PTB(2,:));
 
 figure,
 scatter(l_spectral,s_spectral)
+ylim([0,1])
 
 

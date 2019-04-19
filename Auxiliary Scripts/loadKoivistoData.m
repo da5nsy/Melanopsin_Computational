e1bae03126c0b05e76dae0141c487898eed4035a
da5nsy <dannygarside@outlook.com>
2019-04-19 13:14:53 +0100

    got the extraction script working for the Koivisto data

--- Auxiliary Scripts/loadKoivistoData.m
+++ Auxiliary Scripts/loadKoivistoData.m
@@ -1,5 +1,45 @@
 clear, clc, close all
 
-importdata('C:\Users\cege-user\Dropbox\UCL\Data\Reference Data\Natural Reflectances\natural400_700_5.asc')
+[NUM,TXT,RAW] = xlsread('C:\Users\cege-user\Dropbox\UCL\Data\Reference Data\Natural Reflectances\natural400_700_5.csv');
 
-%not currently working
+%%
+clc
+
+for surf = 1:218
+    clear A B C
+    range = 6*surf-4:6*surf-1;
+    %range = 2:5;
+    
+    for i = 1:length(range)
+        A = char(RAW{range(i),1});
+        B = regexp(A,'\d*','Match');
+        C(i,:) = str2double(B);
+    end
+    C = C';
+    C = C(:);
+    C(end+1) = RAW{6*surf,1};
+    
+    SRF(:,surf) = C;
+    
+end
+
+figure, plot(SRF) %that does not look right, but at least I'm getting at the data
+
+%6x-4 gives the first line
+
+
+
+%% Things that should work but don't:
+
+%data = importdata('C:\Users\cege-user\Dropbox\UCL\Data\Reference Data\Natural Reflectances\natural400_700_5.asc')
+%data = readmatrix('C:\Users\cege-user\Dropbox\UCL\Data\Reference Data\Natural Reflectances\natural400_700_5.asc');
+
+
+
+
+
+
+
+
+
+
